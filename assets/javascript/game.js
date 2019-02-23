1609e9460ab6b8febfa7ca657f867cf1126ecda2
Alexis Villalpando <alexisvillalpando@Trixie-and-Alexis-macbook-pro.local>
2019-02-22 18:07:10 -0800

    game complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,10 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 //   8.)creating variables to hold the number of wins and losses. They start at 0.
 var wins = 0;
 var losses = 0;
-var guesses= 9;
+var attempts = 9;
+var usedArray = [];
+var oneLetter = "";
+
 
 
 //   6.)creates variables that hold references to place in the HTML where we want to display things
@@ -15,41 +18,84 @@ var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-text");
 
+//   4.)randomly chooses a letter from the array
+var one = "";
+
+
+
+function newLetter() {
+
+    oneLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(oneLetter);
+
+}
+newLetter();
+
+
+
 //   2.)function that run whenever a key is pressed
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
     //   3.)determines which key was pressed
     var userGuess = event.key
 
-    //   4.)randomly chooses a letter from the array
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    userChoiceText.textContent = "Guesses so far: " + one;
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        guessesText.textContent = "Guesses left: " + attempts;
+
 
     //   5.)run the code blocks if the user presses .....
-    if (userGuess === "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"){
+    if (userGuess == "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z") {
+        //   9.)if/then statements 
+        //correct guesses
+       
+        if (userGuess !== oneLetter) {
+            attempts--;
+            one += userGuess + ", ";
+        }
 
-    //   9.)if/then statements 
-        if(userGuess === computerGuess){
-
+        if (userGuess == oneLetter) {
             wins++;
-
+            newLetter();
+            attempts=9;
+            one="";
+            usedArray = [];
+            userGuess = "";     
         }
 
-        else{
+        if (attempts == 0) {
             losses++;
+            one="";
+            usedArray = [];
+            attempts = 9;
+            computerChoiceText.textContent = alert("The computer chose: " + oneLetter);
+            userGuess = "";
+            
         }
 
+        //incorrect guesses output
+        if (usedArray >= 0) { }
+        else {
+            usedArray.push(userGuess);
+            document.getElementById("userGuess").innerHTML = usedArray;
+            console.log(usedArray);
+        }
+        //   7.) Displays user and computer guesses.
+        userChoiceText.textContent = "Guesses so far: " + one;
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        guessesText.textContent = "Guesses left: " + attempts;
     }
+   
+
+   
+    
+    
+   
 
 
-    //   7.) Displays user and computer guesses.
-    userChoiceText.textContent = "You chose: " + userGuess;
-    computerChoiceText.textContent = "The computer chose: " + computerGuess;
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
-    guessesText.textContent = "Guesses: " + guesses;
-
-    //   alert the userGuess
-    alert(userGuess);
 
 
 }
